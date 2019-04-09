1919668f166485de874bb634288fef6d0c5a2f84
Adam Bodie <adam.bodie@yahoo.com>
2019-04-08 19:23:41 -0700

    Added functionality to update player name

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import styles from "../css/player.module.scss";
 
 export default class Player extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 		this.state = {
-			value: ''
-		}     
+			value: '',
+			id: this.props.id
+		}
 	this.handleChange = this.handleChange.bind(this);
 	this.handleNameChange = this.handleNameChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
 	this.loop = this.loop.bind(this);
 	}
 	handleChange(e) {
@@ -17,7 +19,10 @@ export default class Player extends Component {
 	handleNameChange(e) {
 		this.setState({value: e.target.value});
 	}
-	
+	handleSubmit(e) {
+		this.setState({id: this.state.value});
+		e.preventDefault();
+	}
 	loop(number) {
 		let numbers = [];
 		for (let i = 1; i < number + 1; i++) numbers.push(i);
@@ -26,13 +31,21 @@ export default class Player extends Component {
 	}
 	
 	render() {
-		const { id, disabled, score } = this.props;
-		const { value } = this.state;
+		const { disabled, score } = this.props;
+		const { value, id } = this.state;
 		return (
 			<div className={styles.player}>
 				<div>
-					<h2 className={styles.playerName}>Player {id}</h2>
-					<input type="text" value={value} onChange={this.handleNameChange} />
+					<form onSubmit={this.handleSubmit}>
+						{!(id === 1 || id === 2) && (<h2 className={styles.playerName}>{id}</h2>)}
+						{(id === 1 || id === 2) && (
+						<div className={styles.form}>
+							<input type="text" value={value} onChange={this.handleNameChange} placeholder="Place your name" />
+							<input type="submit" value="X" />
+						</div>
+						)}
+					</form>
+					
 					<button
 						className={styles.counterAction}
 						onClick={this.handleChange}
